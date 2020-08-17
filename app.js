9d17af060c6127ef4c559349f36b719670922dc3
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-08-17 14:56:51 +0530

    set-up homepage using controller

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
-
+const router = require('./router');
 
 app.use(express.static('public'));
 app.set('Views', 'views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-   res.render('home-guest') 
-})
+app.use('/', router);
 
 app.listen(3000)
